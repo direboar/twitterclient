637dee694e3743fd79e6f90d79f52c60d6e4e4fd
minokuba <gzw04102@nifty.ne.jp>
2015-10-13 22:17:17 +0900

    timeline表示処理を実装（スタイルは未指定）

--- twiterclient/src/main/resources/static/js/app.js
+++ twiterclient/src/main/resources/static/js/app.js
@@ -1,2 +1,4 @@
-var timeline = new minokuba.twitterclient.collection.Timeline();
-timeline.fetch(); //timelineサービスを呼びだす。この時点で通信内容を確認すると、現時点の通信状態を参照できる。
+//var timeline = new minokuba.twitterclient.collection.Timeline();
+//timeline.fetch(); //timelineサービスを呼びだす。この時点で通信内容を確認すると、現時点の通信状態を参照できる。
+
+var TimelineView = new minokuba.twitterclient.view.TimelineView();
